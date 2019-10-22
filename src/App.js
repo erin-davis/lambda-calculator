ccc83fe8ec43f4d14c13e66c83a372359ea66a6c
Erin Davis <davisaerin@gmail.com>
2019-10-22 18:27:56 -0500

    new branch added

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
     <div className="container">
       <Logo />
       <div className="App">
+      <button>1</button>
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       </div>
     </div>
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,14 +1,16 @@
-import React from "react";
-
+import React, {useState} from "react";
+import {numbers} from "../../../data.js";
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
+// example of import from data.js. Note all the ../   This is how we move through folders.
+/*
+import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
 const Numbers = () => {
+  const [numberState, setNumberState] = useState(0);
   // STEP 2 - add the imported data to state
+  
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
